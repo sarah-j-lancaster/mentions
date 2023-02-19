b6cd497607352bff8a11998ca0c4cd2d24a4e74e
Sarah Lancaster <sarah.lancaster@springload.co.nz>
2023-02-19 17:16:12 +1300

    Delete unused var

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,5 @@
 import { MentionViewModel } from "@/pages";
 import { Mention, TwitterMention } from "@/services/api/mentions";
-import { title } from "process";
 
 export const isTwitterMention = (
   b: Mention | TwitterMention
@@ -8,20 +7,6 @@ export const isTwitterMention = (
   return (b as TwitterMention).source_type === "twitter";
 };
 
-const monthShortcutMap = {
-  0: "Jan",
-  1: "Feb",
-  2: "Mar",
-  3: "Apr",
-  4: "May",
-  5: "Jun",
-  6: "Jul",
-  7: "Aug",
-  8: "Sep",
-  9: "Oct",
-  10: "Nov",
-  11: "Dec",
-};
 export const formatDate = (rawDate: string): string => {
   const date = new Date(rawDate);
   return date.toLocaleDateString("en-GB", {
